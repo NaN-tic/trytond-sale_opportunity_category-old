033b1abb9bcf29618050a55f0dc9a1666b17f44d
resteve <resteve@zikzakmedia.com>
2015-12-01 09:44:19 +0100

    Tests

--- tests/test_sale_opportunity_category.py
+++ tests/test_sale_opportunity_category.py
@@ -1,27 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the sale_opportunity_category module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_opportunity_category')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_opportunity_category')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SaleOpportunityCategoryTestCase(ModuleTestCase):
+    'Test Sale Opportunity Category module'
+    module = 'sale_opportunity_category'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        SaleOpportunityCategoryTestCase))
     return suite
